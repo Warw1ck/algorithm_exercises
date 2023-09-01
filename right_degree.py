219c797b58bcc18c5abc0ad09bc501562dfadc67
Warw1ck <bradvata15@gmail.com>
2023-09-01 23:24:21 +0300

    Version 9

---
+++ right_degree.py
@@ -0,0 +1,3 @@
+def check_right_degree_of_number(number, degree):
+    if (number and (number-1) == 0) and  number != 0:
+        print('True')
---
+++ stairs.py
@@ -0,0 +1,10 @@
+def number_stairs(stairs):
+    stairs_combination = 0
+    first_number = 1
+    second_number = 0
+
+    for i in range(2, stairs):
+        pass
+
+
+
